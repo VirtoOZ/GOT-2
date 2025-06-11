f27c3965b37e62e930ec65168cbe86943c7f99af
VirtoOZ <CenTROY@yandex.ru>
2025-06-11 15:14:33 +0300

    подготовительный этап

---
+++ src/js/react/components/app/app.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import RandChar from "../randChar/randChar.jsx";
+import './app.scss'
+
+export default class App extends Component {
+
+
+
+	render() {
+
+		return (
+			<>
+				<main className="page">
+					<section className="page__main main">
+					</section>
+					<section className="page__main main">
+					</section>
+				</main>
+			</>
+		)
+	}
+}
---
+++ src/js/react/components/randChar/randChar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class RandChar extends Component {
+
+	state = { data: '', }
+
+	render() {
+		return (
+			null
+		);
+	}
+}
