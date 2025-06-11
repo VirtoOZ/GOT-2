70d4b499cb90843e9b4fb77725e7ee2620eccd02
VirtoOZ <CenTROY@yandex.ru>
2025-06-11 15:32:07 +0300

    формирование HTML...

--- src/js/react/components/app/app.jsx
+++ src/js/react/components/app/app.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import RandChar from "../randChar/randChar.jsx";
-import './app.scss'
+import './app.scss';
+import img from "./got.jpeg";
+
 
 export default class App extends Component {
 
@@ -11,10 +13,19 @@ export default class App extends Component {
 		return (
 			<>
 				<main className="page">
-					<section className="page__main main">
+					<section className="random-char random-char__section page__section">
+						<div className="random-char__container">
+							<ul className="random-char__list list">
+								<li className="random-char__item list__item">
+									<span className="random-char__label list__label"></span>
+									<span className="random-char__value list__value"></span>
+								</li>
+							</ul>
+						</div>
 					</section>
-					<section className="page__main main">
-					</section>
+					<div className="page__bg">
+						<img src={img} alt="bg"></img>
+					</div>
 				</main>
 			</>
 		)
